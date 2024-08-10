823ca98658f9fd3da51977a8c6c4faac25819704
IrynaRybka <irarybka@gmail.com>
2024-08-10 13:06:03 +0300

    style

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
 
+
+// import { useState } from "react";
 import Icon from "../../ui/Icon";
 
 import styles from "./NavBar.module.css";
@@ -9,26 +11,37 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({hendlerScrollToSection}) => {
 
+//   const [isActive, setIsActive] = useState(styles.active)
+//  function activeNavLink(path) {
+
+//   if (isActive) {
+//     setIsActive()
+//   }
+//  }
+  
+// let styleActive;
+
+// const link = <a className={styles.active} href={href}>{children}</a>
 
   return (
     <nav>
       <ul className={styles.nav_list}>
         <li className={styles.nav_list_element}>
-          <a href="./Header.tsx">
+          <a href="/">
             <Icon id="icon-logo" width={173} height={65} />
           </a>
         </li>
         <li className={styles.nav_list_element}>
-          <a href="#services" onClick={hendlerScrollToSection}>Послуги</a>
+          <a  href="#services" onClick={hendlerScrollToSection}>Послуги</a>
         </li>
         <li className={styles.nav_list_element}>
-          <a href="#advantages" onClick={hendlerScrollToSection}>Переваги</a>
+          <a  href="#advantages" onClick={hendlerScrollToSection}>Переваги</a>
         </li>
         <li className={styles.nav_list_element}>
-          <a href="#appointment" onClick={hendlerScrollToSection}>Контакти</a>
+          <a  href="#appointment" onClick={hendlerScrollToSection}>Контакти</a>
         </li>
         <li className={styles.nav_list_element}>
-          <a href="#about" onClick={hendlerScrollToSection}>Про майстра</a>
+          <a  href="#about" onClick={hendlerScrollToSection}>Про майстра</a>
         </li>
         <li className={styles.nav_list_element}>
           <a href="tel:0637967425">063 796 74 25</a>
